857b7ef94d6c63de4057e390a470721c905dfa02
Aleksey S <93787802+AlekseySamokhin@users.noreply.github.com>
2021-11-16 18:49:47 +0300

    Add files via upload

---
+++ scripts/wis/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $(".slider").owlCarousel({
+    items: 1,
+    dots: true,
+    dotsClass: "slider-dots",
+    nav: false,
+    loop: false,
+    autoplay: false,
+    center: false
+  });
+});
